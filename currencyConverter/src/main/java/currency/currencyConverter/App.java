262ad347172a9e54372e0c9720609a8f139f7f17
Ritesh9354 <132083353+Ritesh9354@users.noreply.github.com>
2023-09-04 15:51:29 +0530

    Add files via upload

---
+++ currencyConverter/src/main/java/currency/currencyConverter/App.java
@@ -0,0 +1,13 @@
+package currency.currencyConverter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
